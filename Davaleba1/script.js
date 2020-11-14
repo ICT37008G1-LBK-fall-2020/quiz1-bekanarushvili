446d2d9c0b0b8376cd8434c9b0e9507d14311f14
bekanarushvili <github@beka.com.ge>
2020-11-14 12:05:10 +0400

    Quiz 1 - Task 1

---
+++ Davaleba1/script.js
@@ -0,0 +1,21 @@
+// დაწერეთ ფუნქცია, რომელიც მიღებული რიცხვების მასივიდან დააბრუნებს უდიდეს ელემენტს
+// function getMaxNumber(numbersArray){ }
+
+let MynumbersArray = [-1, 3, 4, 6, 5, 21, 33, 34, 45, 55, 96, 54.3, 65];
+
+function getMaxNumber(numbersArray)
+{ 
+    let result = numbersArray[0];
+    for (let i=0; i<numbersArray.length; i++)
+    {
+        if (numbersArray[i]>result)
+        {
+            result = numbersArray[i];
+        }
+    }
+    return result;
+}
+
+
+let answer = getMaxNumber (MynumbersArray);
+alert (answer);
